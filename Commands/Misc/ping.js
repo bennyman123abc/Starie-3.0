9ee0c452d38a2f44c8f2610c99df378955758c53
bennyman123abc <bennyman123abc@hotmail.com>
2017-10-17 20:13:48 -0400

    Add Commands !add and !say

--- Commands/Misc/ping.js
+++
@@ -1,7 +0,0 @@
-//permission: Everyone;
-//usage: None;
-var rembed = new Discord.RichEmbed();
-rembed.setAuthor(client.user.username, client.user.avatarURL);
-rembed.setDescription("Pong!");
-rembed = rembed.addField("Ping", Math.floor(client.ping/12));
-message.channel.send({ embed : rembed});
