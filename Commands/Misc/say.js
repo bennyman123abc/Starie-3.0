99b78b9d468279c74b764aa1173a7a681af4cf07
matthe815 <matthe815gameproduction@gmail.com>
2017-10-20 21:38:48 -0400

    Update say.js
    Added comments and changed some of the general code.

--- Commands/Misc/say.js
+++ Commands/Misc/say.js
@@ -1,8 +1,14 @@
 //permission: Everyone;
 //usage: <text>;
+// Check how many arguments exist.
 if (args.length != 1){
+    // If there's less than or more than 1, stop it.
     message.reply("Only 1 argument is allowed!")
 }
 else{
-    message.reply(args[0])
+    // Reply the text.
+    message.reply(args.join().replace(","," "));
+    
+    // Delete the previous message.
+    message.delete();
 }
