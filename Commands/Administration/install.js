7324e6135ae5a0091b0c84bf3cbd66ff6712f159
matthe815 <matthe815gameproduction@gmail.com>
2017-10-23 13:38:56 -0400

    Create install.js

---
+++ Commands/Administration/install.js
@@ -0,0 +1,5 @@
+// Welcome them.
+message.reply("Welcome! And thanks for downloading Matthe815's Starie 3.0 software.");
+message.channel.send("Most of the installation process will be done in the console.");
+message.channel.send("To start, setup your first owner. To do this, enter the userID of the user whom you want to be the first `Super Owner` into the console.");
+eval("../../../Install/installer.js");
