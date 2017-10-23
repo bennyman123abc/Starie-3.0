993cb90c9359736464778f19389d1b8afd23d6cf
matthe815 <matthe815gameproduction@gmail.com>
2017-10-23 13:08:26 -0400

    Update ping.js
    Changed "avatarURL" to "displayAvatarURL". Added a few comments.

--- Commands/General/ping.js
+++ Commands/General/ping.js
@@ -1,7 +1,13 @@
 //permission: Everyone;
 //usage: None;
+
+// Create the embed.
 var rembed = new Discord.RichEmbed();
-rembed.setAuthor(client.user.username, client.user.avatarURL);
+
+// Set author, description, and add a field.
+rembed.setAuthor(client.user.username, client.user.displayAvatarURL);
 rembed.setDescription("Pong!");
-rembed = rembed.addField("Ping", Math.floor(client.ping/12));
+rembed = rembed.addField("Ping", Math.floor(client.ping));
+
+// Post it.
 message.channel.send({ embed : rembed});
