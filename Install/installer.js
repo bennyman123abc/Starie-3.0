429b57c51695939c5f851fc42154ac3ef7e8c038
matthe815 <matthe815gameproduction@gmail.com>
2017-10-23 14:58:05 -0400

    Update installer.js
    Finished product.

--- Install/installer.js
+++ Install/installer.js
@@ -4,56 +4,46 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-var loop = true;
-
-while (loop) {
-    loop = false;
-    rl.question('Input the ID of the user whom you want to be owner:\n', (answer) => {
-        if (answer.length == 18)
-        {
-            fs.open(`../../database/Users/${answer}.json`, 'w+', () => {
-                fs.writeFileSync(`././database/Users/${answer}.json`, '{"Whitelisted" : true, "Blacklisted" : false, "Permissions" : ["Alpha","Beta"], "Prefix" : "?", "Owner" : true, "canBypassPermissions" : true}');
-            });
+rl.question('Input the ID of the user whom you want to be owner:\n', (answer) => {
+    if (answer.length == 18)
+    {
+        fs.open(`../../database/Users/${answer}.json`, 'w+', () => {
+            fs.writeFileSync(`././database/Users/${answer}.json`, '{"Whitelisted" : true, "Blacklisted" : false, "Permissions" : ["Alpha","Beta"], "Prefix" : "?", "Owner" : true, "canBypassPermissions" : true}');
+        });
                     
-	          console.log(`Set the Super Owner to ${answer}.`);
-        }
-        else
-        {
-            console.log("This ID isn't valid, it MUST be exactly 18 characters.");
-            loop = true;
-        }
-    });
-}
-
-loop = true;
-
-while (loop) 
-{
-    loop = false;
-    rl.question("Input a default prefix.", (answer) => {
-        if (answer.length > 0 || answer.length < 3)
-        {
-            var conf = JSON.parse(fs.readFileSync("././Config/config.json"));
-            conf['basic']['defaultPrefix'] = answer;
-            fs.writeFileSync("././Config/config.json", JSON.stringify(conf));
-			console.log(`Set default prefix to ${answer}.`);
-        }
-        else
-        {
-            console.log("The default prefix MUST be 3 characters or less."); 
-        }
-    })
-}
-
-loop = true;
-
-while (loop) 
-{
-    loop = false;
-    rl.question("Input a startup status message.", (answer) => {
-        var conf = JSON.parse(fs.readFileSync("././Config/config.json"));
-        conf['basic']['game'] = answer;
-        fs.writeFileSync("././Config/config.json", JSON.stringify(conf));
-		console.log(`Set start up status message to ${answer}.`);
-    })
-}
+	    console.log(`Set the Super Owner to ${answer}.`);
+		rl.question("Input a default prefix:\n", (answer) => {
+			if (answer.length > 0 || answer.length < 3)
+			{
+				var conf = JSON.parse(fs.readFileSync("././Config/config.json"));
+				conf['basic']['defaultPrefix'] = answer;
+				fs.writeFileSync("././Config/config.json", JSON.stringify(conf));
+				console.log(`Set default prefix to ${answer}.`);
+			
+				rl.question("Input a startup status message:\n", (answer) => {
+					var conf = JSON.parse(fs.readFileSync("././Config/config.json"));
+					conf['basic']['game'] = answer;
+					fs.writeFileSync("././Config/config.json", JSON.stringify(conf));
+					console.log(`Set start up status message to ${answer}.`);
+					console.log("Starie set up complete, please restart the application for changes to show up!");
+					fs.unlinkSync("././Install/installer.js")
+					fs.rmdirSync("././Install");
+				});
+			}
+			else
+			{
+				console.log("Invalid default prefix length, using !.");
+				var conf = JSON.parse(fs.readFileSync("././Config/config.json"));
+				conf['basic']['defaultPrefix'] = "!";
+				fs.writeFileSync("././Config/config.json", JSON.stringify(conf));
+			}
+	    });
+	}
+    else
+    {
+        console.log("Invalid ID, using the !install sender as the owner.");
+		fs.open(`../../database/Users/${message.author.id}.json`, 'w+', () => {
+            fs.writeFileSync(`././database/Users/${message.author.id}.json`, '{"Whitelisted" : true, "Blacklisted" : false, "Permissions" : ["Alpha","Beta"], "Prefix" : "?", "Owner" : true, "canBypassPermissions" : true}');
+        });
+    }
+});
