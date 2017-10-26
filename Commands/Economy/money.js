baf98fccd3ab5fd69c86dde55d245a387da15a99
matthe815 <matthe815gameproduction@gmail.com>
2017-10-25 20:42:45 -0400

    Update money.js

--- Commands/Economy/money.js
+++ Commands/Economy/money.js
@@ -5,36 +5,8 @@
 // Get the server.
 var server = JSON.parse(String(fs.readFileSync(`./././${config["local"]["serverSettings"]}/${message.guild.id}.json`)));
 
-
-// Confirm which module is active.
-if(server["modules"]["LOCAL_ECONOMY"] == true)
-{
-    // Check how many arguments there are.
-    if (args != null && args.length == 1)
-	{
-	    var user = getUserResolvable(args[0], message.guild.id);
-	    if (server["economy"][user.id] == undefined)
-		{
-		   server["economy"][user.id] = 0;
-		   fs.writeFileSync(`./././${config["local"]["serverSettings"]}/${message.guild.id}.json`, JSON.stringify(server));
-		}
-		
-	    console.log(server["economy"][user.id]);
-		message.reply(`${user.tag} has $${server["economy"][user.id]} in their wallet.`);
-	}
-	else if (args == null)
-	{
-	    if (server["economy"][message.author.id] == undefined)
-		{
-		   server["economy"][message.author.id] = 0;
-		   fs.writeFileSync(`./././${config["local"]["serverSettings"]}/${message.guild.id}.json`, JSON.stringify(server));
-		}
-		
-	    console.log(server["economy"][message.author.id]);
-		message.reply(`${message.author.tag} has $${server["economy"][message.author.id]} in their wallet.`);
-	}
-}
-else if (server["modules"]["GLOBAL_ECONOMY"] == true)
+// Check if it's enabled.
+if (server["modules"]["GLOBAL_ECONOMY"] == true)
 {
     // Check how many arguments there are.
     if (args != null && args.length == 1)
