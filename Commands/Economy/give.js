dd95135b45adcc583589ddf3c4a47175bfb1506f
matthe815 <matthe815gameproduction@gmail.com>
2017-10-25 21:24:37 -0400

    Update give.js

--- Commands/Economy/give.js
+++ Commands/Economy/give.js
@@ -1,40 +1,32 @@
 //permission: Everyone;
 //usage: <amount> | <userResolve>;
-//module: ECONOMY;
-
+//module: GLOBAL_ECONOMY;
 // Make sure there are 2 arguments
-if (args != 2) {
+if (args.length == 2) {
+
+    if (isOwner(message.author.id, message.guild.id)) {
+        // Get the server.
+        var server = JSON.parse(String(fs.readFileSync(`./././${config["local"]["serverSettings"]}/${message.guild.id}.json`)));
+
+        // Get the user and their information
+        var user = getUserResolvable(args[1], message.guild.id);
+
+        var userInfo = JSON.parse(String(fs.readFileSync(`./././${config["local"]["userSettings"]}/${user.id}.json`)));
+
+        // Get the user's current amount of money
+        if (userInfo["money"] == undefined) {
+            var currentMoney = 0;
+        } else {
+            var currentMoney = userInfo["money"];
+        }
+
+        // Add the money
+        userInfo["money"] = currentMoney + args[0];
+        fs.writeFileSync(`./././${config["local"]["userSettings"]}/${user.id}.json`, JSON.stringify(userInfo));
+
+        // Send confirmation message
+        message.reply("You now have $" + String(userInfo["money"]) + ".")
+    }
+} else {
     message.reply(errorStrings["commands"]["arguments"]);
-    return;
 }
-
-// Get the server.
-var server = JSON.parse(String(fs.readFileSync(`./././${config["local"]["serverSettings"]}/${message.guild.id}.json`)));
-
-// Make sure that the module is enabled before continuing. If it isn't, we just quit from here.
-if (server["modules"]["GLOBAL_ECONOMY"] == false)
-{
-    message.reply(String(errorStrings['modules']['globalEconomy']));
-    return;
-}
-
-// Get the user and their information
-var user = getUserResolvable(args[1], message.guild.id);
-
-var userInfo = JSON.parse(String(fs.readFileSync(`./././${config["local"]["userSettings"]}/${user.id}.json`)));
-
-// Get the user's current amount of money
-if (userInfo["money"] == undefined){
-    var currentMoney = 0;
-}
-
-else {
-    var currentMoney = userInfo["money"];
-}
-
-// Add the money
-userInfo["money"] = currentMoney + args[0];
-fs.writeFileSync(`./././${config["local"]["userSettings"]}/${user.id}.json`, JSON.stringify(userInfo));
-
-// Send confirmation message
-message.reply("You now have $" + String(userInfo["money"]) + ".")
