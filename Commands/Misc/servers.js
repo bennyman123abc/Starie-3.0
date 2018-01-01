e711867060d5f6c0ce7f182994fef4148db57c32
bennyman123abc <bennyman123abc@hotmail.com>
2018-01-01 00:52:55 -0500

    Update servers.js

--- Commands/Misc/servers.js
+++ Commands/Misc/servers.js
@@ -3,14 +3,15 @@
 
 var totalCount = 0;
 var embString = "";
-var emb = new Discord.RichEmbed();
+var emb = new Discord.MessageEmbed();
 
-client.guilds.forEach((server) =>
+client.guilds.forEach((server) => {
     totalCount += server.memberCount;
     embString += `${server.name} | ${server.memberCount} user(s)\n`;
 })
 
 emb.addField("Servers", embString);
 emb.setFooter(`Serving ${totalCount} user(s) in ${client.guilds.size} server(s).`)
+emb.setColor("GREEN");
 
 message.channel.send({ embed : emb });
